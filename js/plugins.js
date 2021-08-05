afa1199d8a3119ec25ad1c8e7eb482f7e2ed24c0
Liviu Andrei <liviuandrei.dev@gmail.com>
2021-08-05 18:33:04 +0300

    Refactor everything

--- js/plugins.js
+++
@@ -1,6 +0,0 @@
-const gameProjs = document.getElementById('game-projs');
-
-gameProjs.addEventListener('click', e => {
-    document.querySelector('.game-projs').classList.toggle('show');
-    document.querySelector('.web-projs').classList.toggle('hide');
-})
